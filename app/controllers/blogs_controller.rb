a5c9d607e184a9c03f96a790901cef0642179f89
chiku3 <p314chiku.y@gmail.com>
2021-12-12 03:16:16 +0000

    12/12

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,22 +1,24 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
     @blog = Blog.new
   end
 
-　def create
-　  blog = Blog.new(blog_params)
+  def create
+    blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
-　  
-　end
+    redirect_to blogs_path(blog.id)
+  end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
   
   private
